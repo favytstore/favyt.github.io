5fc9f14e7c114342775e31775175c488e340cd3c
amrendrakind <vision2010@gmail.com>
2021-10-13 16:31:57 +0530

    Favyt: App

--- src/App.js
+++ src/App.js
@@ -2,24 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import {BrouserRouter, Switch, Route, NavLink} from 'react-router-dom';
+import Login from './frontend/componets/Login';
+import Dashboard from './frontend/componets/Dashboard';
+import Home from './frontend/componets/Home';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Your Favyt is evolving : Stay tune with Favorite Collections!!
-        </p>
-        <a
-          className="App-link"
-          href="#"
-          rel="noopener noreferrer"
-        >
-          Favyt
-        </a>
-      </header>
+      <BrouserRouter>
+        <div>        
+          <div className="header">
+            <NavLink exact activeClassName = 'active' to ='/'>Home</NavLink>
+            <NavLink exact activeClassName = 'active' to ='/login'>Login</NavLink> <small>(Access without token only)</small>
+            <NavLink exact activeClassName = 'active' to ='/dashboard'>Dashboard</NavLink><small>(Access with token only)</small>
+          </div>
+          <div className="contect">
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route path='/login' component={Login} />
+                <Route path='/dashboard' component={Dashboard} />
+              </Switch>
+          </div>
+        </div>
+      </BrouserRouter>
     </div>
   );
 }
