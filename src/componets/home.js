7d648dbb58370e2e8a1a39479ad52964e093d936
amrendrakind <vision2010@gmail.com>
2021-10-13 11:11:54 +0530

    Favyt:Home

---
+++ src/componets/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Home(){
+    return(
+        <div>
+            <p>Welcome to Favyt : Your Favorite Collection!!!</p>
+        </div>
+    );
+}
+export default Home;
