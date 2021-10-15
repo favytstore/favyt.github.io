e284b99e090bfef84f226b6e4b3faaa7ec947308
amrendrakind <vision2010@gmail.com>
2021-10-15 23:59:26 +0530

    Favyt: Updated

--- src/frontend/componets/Home.js
+++ src/frontend/componets/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 function Home(){
     return(
         <div>
-            <p><h1 style="color: blue">Welcome to Favyt : Your Favorite Collection!!!</h1></p>
+            <p><h1 style="color: blue;">Welcome to Favyt : Your Favorite Collection!!!</h1></p>
         </div>
     );
 }
