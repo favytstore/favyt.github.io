e36ccf45959d27c4db6bb6b94a433defdfd0dfaf
amrendrakind <vision2010@gmail.com>
2021-10-16 14:38:54 +0530

    Backend Design

--- src/backend/server.js
+++ src/backend/server.js
@@ -11,3 +11,6 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended : true}))
+
+
+app.listen(port,()=> {console.log("Server started at " +port)})
