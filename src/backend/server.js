80e785a86edf0a69d0b83a9567929c4261993b5f
amrendrakind <vision2010@gmail.com>
2021-10-16 18:55:08 +0530

    Backend Design

--- src/backend/server.js
+++ src/backend/server.js
@@ -11,6 +11,8 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended : true}))
-
+app.get('/', (req, res) => {
+    res.send('Welcome to the Favyt Store! - ' + req.user.name);
+  });
 
 app.listen(port,()=> {console.log("Server started at " +port)})
