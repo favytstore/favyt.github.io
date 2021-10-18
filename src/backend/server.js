3fc89c47a2928d962907162fb9fb4d9923283b9a
amrendrakind <vision2010@gmail.com>
2021-10-19 00:11:51 +0530

    Backend Design

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const Connct_Mongo_DB = require('./dbConfig')
+const fileUpload = require('express-fileupload')
 
 const PORT = process.env.PORT || 4040;
 
-// Initialisation of Mongodb connection
-Connct_Mongo_DB();
+Connct_Mongo_DB(); // Initialisation of Mongodb connection
 
 const app = express()
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+app.use(fileUpload({ useTempFiles: true }))
 
 app.use(express.json());  // Middleware
+
+app.use('/upload',require('./routes/upload'));
 
 app.listen(PORT,console.log(`Server is running on ${PORT}`))
